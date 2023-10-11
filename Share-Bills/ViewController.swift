a06c320281dc903c947ed5defc07ef42c2cb47d2
Armando Cáceres <armando.snoop@gmail.com>
2023-10-11 16:09:34 +0200

    Initial Commit

---
+++ Share-Bills/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Share-Bills
+//
+//  Created by Jesús Armando Cáceres Vilchez on 11/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
