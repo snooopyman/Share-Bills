424dff97a951f637e33913bc21fb6c0903117f94
Armando Cáceres <armando.snoop@gmail.com>
2023-10-12 20:40:27 +0200

    Adding Buttons, Colours, Image and reusable componentes

--- Share-Bills/Controller/WelcomeViewController.swift
+++ Share-Bills/Controller/WelcomeViewController.swift
@@ -9,11 +9,12 @@ import UIKit
 
 class WelcomeViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    override func loadView() {
+        self.view = WelcomeView()
     }
 
-
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
 }
 
