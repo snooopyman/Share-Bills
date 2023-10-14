9e8e0afecdddda393ce903fcfec93aba8aeab89b
Armando Cáceres <armando.snoop@gmail.com>
2023-10-14 14:20:09 +0200

    Made WelcomeView and fix constraints

---
+++ Share-Bills/Controller/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  Share-Bills
+//
+//  Created by Jesús Armando Cáceres Vilchez on 14/10/23.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .purple
+    }
+}
